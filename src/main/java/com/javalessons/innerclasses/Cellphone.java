a11894ce8a6121da82ca73eed2c9675da39f772a
fogma <fogmatic@gmail.com>
2020-07-24 18:52:52 +0300

    update

--- src/main/java/com/javalessons/innerclasses/Cellphone.java
+++ src/main/java/com/javalessons/innerclasses/Cellphone.java
@@ -1,16 +1,10 @@
-package com.javalessons.innerclasses;
+package main.java.com.javalessons.innerclasses;
 
-class CellPhone {
+public class CellPhone {
 
     private String make;
     private String model;
     private Display display;
-    private RadioModule gsm;
-    private AbstractPhoneButton button;
-
-    public interface AbstractPhoneButton{
-        void click();
-    }
 
     public CellPhone(String make, String model) {
         this.make = make;
@@ -19,22 +13,6 @@ class CellPhone {
 
     public void turnOn() {
         initDisplay();
-        gsm = new RadioModule();
-        initButton();
-    }
-
-    public void initButton() {
-    button = new AbstractPhoneButton() {
-        @Override
-        public void click() {
-            System.out.println("button clicked");
-            }
-        };
-    }
-
-    public void call(String number) {
-        button.click();
-        gsm.call(number);
     }
 
     private void initDisplay () {
