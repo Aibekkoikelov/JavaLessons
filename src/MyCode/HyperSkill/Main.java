b415b01641f91c6066e429d2aaebe68a2d62171c
fogma <fogmatic@gmail.com>
2020-07-31 16:53:57 +0300

    30.07.2020 update strings

--- src/MyCode/HyperSkill/Main.java
+++ src/MyCode/HyperSkill/Main.java
@@ -1,5 +1,6 @@
 package MyCode.HyperSkill;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -11,14 +12,33 @@ public class Main {
 //        System.out.println(Double.NaN - Double.NaN);
 
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine().toLowerCase();
-        double result = (double) (((str.length() - str.replaceAll("[g|c]","").length()) / str.length()) * 100);
-        System.out.println(result);
+        String str = sc.nextLine();
+        str = str.replaceAll("[aeiouy]", "1");
+        str = str.replaceAll("[bcdfghjklmnpqrstvwxz]", "0");
+        String[] newStr1 = str.split("0");
+        String[] newStr2 = str.split("1");
+        int n = 0;
+        int m = 0;
+
+//        System.out.println(Arrays.toString(newStr1));
+//        System.out.println(Arrays.toString(newStr2));
+
+        for (String s : newStr1) {
+            if (s.length() >= 3) {
+                n += s.length() % 2 + s.length() / 2 - 1;
+            }
+        }
+
+
+        for (String s : newStr2) {
+            if (s.length() >= 3) {
+                m += s.length() % 2 + s.length() / 2 - 1;
+            }
+        }
+
+        System.out.println(m + n);
     }
-
 }
-
-
 
 //
 //        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
