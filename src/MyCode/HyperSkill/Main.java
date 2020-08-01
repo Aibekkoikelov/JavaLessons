374432b50a1550f913fe42f10677afafcdbf21f8
fogma <fogmatic@gmail.com>
2020-08-01 20:25:51 +0300

    01.08.2020. Strings

--- src/MyCode/HyperSkill/Main.java
+++ src/MyCode/HyperSkill/Main.java
@@ -11,10 +11,20 @@ public class Main {
 //        System.out.println(Double.NaN - Double.NaN);
 
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine().toLowerCase();
-        double a = str.replaceAll("[g|c]","").length();
-        double result = (((str.length() - a) / str.length()) * 100);
-        System.out.println(result);
+        String str = sc.nextLine();
+        String newStr = "";
+        int i = 0;
+        for (; i < str.length(); i++) {
+            int count = 1;
+            for (int j = i+1; j < str.length(); j++) {
+                if (str.charAt(i) == str.charAt(j)) {
+                    count++;
+                    i++;
+                }
+            }
+            newStr = newStr + str.charAt(i) + count;
+        }
+        System.out.println(newStr);
     }
 
 }
