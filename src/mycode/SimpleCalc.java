630c460d13c1fe9b69cae78c5dc2f8a6fbc326f9
fogma <fogmatic@gmail.com>
2020-09-23 21:51:52 +0300

    init commit

--- src/mycode/SimpleCalc.java
+++ src/mycode/SimpleCalc.java
@@ -14,83 +14,85 @@ public class SimpleCalc {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         while (true) {
-
             String input = reader.readLine();
             String[] inputLine = input.trim().split("\\s+");
             for (String s : inputLine)
                 try {
-                    switch (s) {
-                        case "+":
-                            addToStackHistory(stack);
-                            sum(stack);
-                            break;
-                        case "-":
-                            addToStackHistory(stack);
-                            subtraction(stack);
-                            break;
-                        case "*":
-                            addToStackHistory(stack);
-                            multiply(stack);
-                            break;
-                        case "/":
-                            addToStackHistory(stack);
-                            division(stack);
-                            break;
-                        case "sqrt":
-                            addToStackHistory(stack);
-                            squareRoot(stack);
-                            break;
-                        case "undo":
-                            stack = undoOperation(newStack);
-                            break;
-                        case "clear":
-                            addToStackHistory(stack);
-                            stack.clear();
-                            break;
-                        default:
-                            addToStackHistory(stack);
-                            addToStack(stack, s);
-                    }
-                }
-                catch (EmptyStackException e) {
+                    calc(s);
+                } catch (EmptyStackException e) {
                     System.out.println("куда ты лезешь, укурок? нет столько чисел");
                     printStack(stack);
-                    break;
+//                    break;
                 }
             printStack(stack);
         }
     }
 
+    public static void calc(String s) {
+        switch (s) {
+            case "+":
+                addToStackHistory(stack);
+                sum(stack);
+                break;
+            case "-":
+                addToStackHistory(stack);
+                subtraction(stack);
+                break;
+            case "*":
+                addToStackHistory(stack);
+                multiply(stack);
+                break;
+            case "/":
+                addToStackHistory(stack);
+                division(stack);
+                break;
+            case "sqrt":
+                addToStackHistory(stack);
+                squareRoot(stack);
+                break;
+            case "undo":
+                stack = undoOperation(newStack);
+                break;
+            case "clear":
+                addToStackHistory(stack);
+                stack.clear();
+                break;
+            default:
+                addToStackHistory(stack);
+                addToStack(stack, s);
+        }
+    }
+
     public static void addToStack(Stack<Double> stack, String a) {
 
-            double number = Integer.parseInt(a);
-            stack.push(number);
+        double number = Integer.parseInt(a);
+        stack.push(number);
     }
 
     public static void sum(Stack<Double> stack) {
-            stack.push(stack.pop() + stack.pop());
+        stack.push(stack.pop() + stack.pop());
 
     }
 
     public static void subtraction(Stack<Double> stack) {
-            double n1 = stack.pop();
-            double n2 = stack.pop();
-            stack.push(n2 - n1);
+        double n1 = stack.pop();
+        double n2 = stack.pop();
+        stack.push(n2 - n1);
     }
 
     public static void multiply(Stack<Double> stack) {
-            stack.push(stack.pop() * stack.pop());
+        stack.push(stack.pop() * stack.pop());
     }
 
     public static void division(Stack<Double> stack) {
-            double n1 = stack.pop();
-            double n2 = stack.pop();
-            stack.push(n2 / n1);
+        double n1 = stack.pop();
+        double n2 = stack.pop();
+        stack.push(n2 / n1);
     }
 
     public static void squareRoot(Stack<Double> stack) {
-            double n = Math.sqrt(stack.pop());
-            stack.push(n);
+        double n = Math.sqrt(stack.pop());
+        stack.push(n);
     }
 
     public static Stack<Double> undoOperation(Stack<Stack<Double>> newStack) {
