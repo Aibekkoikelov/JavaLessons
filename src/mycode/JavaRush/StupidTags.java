95e28fb08b41b604c5d09c95e0607bf8dc81ec79
fogma <fogmatic@gmail.com>
2020-11-18 00:40:28 +0300

    init commit

--- src/mycode/JavaRush/StupidTags.java
+++ src/mycode/JavaRush/StupidTags.java
@@ -4,10 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -50,8 +47,10 @@ public class StupidTags {
     }
 
     public static String findFirstTag(String line, List<Tag> tags) {
+        Stack<Tag> taggs = new Stack<>();
+        for (Tag t : taggs) {
 
-
+        }
     }
 
     static class Tag {
